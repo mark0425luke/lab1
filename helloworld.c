7c79f49b4db2457f9b9fd498900511a604859ca6
mark0425luke <45451299+mark0425luke@users.noreply.github.com>
2020-03-05 00:38:38 +0800

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -5,9 +5,9 @@ int main (void) {
 
     int i;
 
-    // print hello world for 9 times
+    // print hello world for 5 times
 
-    for (i=1; i<10; ++i) {
+    for (i=1; i<6; ++i) {
 
         printf("hello world %d times!\n", i);
 
